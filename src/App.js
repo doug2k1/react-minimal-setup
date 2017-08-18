2edd9925cdf121cf4db60262362202505d10354f
Douglas Matoso <doug2k1@gmail.com>
2017-08-17 23:21:57 -0300

    Standard format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
@@ -9,7 +9,7 @@ class App extends React.Component {
     }
   }
 
-  render() {
+  render () {
     return (
       <div>
         <h1>App component</h1>
